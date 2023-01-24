a65e13d8c7cfc47a3799caae13248efa588e1314
Natalya Shymkova <Natalia.Matiishena@yandex.ru>
2023-01-24 07:31:18 +0300

    Edit app header

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -4,27 +4,30 @@ import {
   ListIcon,
   ProfileIcon
 } from '@ya.praktikum/react-developer-burger-ui-components';
-import appHeaderStyle from './appHeader.module.css';
+import appHeaderStyle from './AppHeader.module.css';
 
 function AppHeader() {
   return (
-    <header className={appHeaderStyle.header + ' p-10'}>
-      <nav>
+    <header className={appHeaderStyle.header + ' pt-10 pb-5 pl-5 pr-5'}>
+      <nav className="pb-4 pt-4">
         <ul className={appHeaderStyle.navigation}>
-          <li className={appHeaderStyle.navigation__item}>
+          <li className={appHeaderStyle.navigation__item + ' pb-4 pt-4 pr-5 pl-5'}>
             <BurgerIcon type="primary" />
-            <span className="pl-3">Конструктор</span>
+            <span className="pl-2">Конструктор</span>
           </li>
-          <li className={appHeaderStyle.navigation__item}>
+          <li className={appHeaderStyle.navigation__item + ' pb-4 pt-4 pr-5 pl-5'}>
             <ListIcon type="primary" />
-            <span className="pl-3">Лента заказов</span>
+            <span className="pl-2">Лента заказов</span>
           </li>
         </ul>
       </nav>
-      <Logo className={appHeaderStyle.logo} />
-      <a href="#" className={appHeaderStyle.profile}>
+      <div className={appHeaderStyle.logo}>
+        <Logo />
+      </div>
+
+      <a href="#" className={appHeaderStyle.profile + ' pb-4 pt-4'}>
         <ProfileIcon type="primary" />
-        <span className="pl-3">Личный кабинет</span>
+        <span className="pl-2">Личный кабинет</span>
       </a>
     </header>
   );
