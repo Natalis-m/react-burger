fd754402ca4618a50136e7206729366ae4a0ee59
Natalya Shymkova <Natalia.Matiishena@yandex.ru>
2023-01-24 07:30:40 +0300

    Editing common styles

--- src/App.js
+++ src/App.js
@@ -2,24 +2,50 @@ import { useEffect, useState } from 'react';
 
 import BurgerConstructor from './components/BurgerConstructor/BurgerConstructor';
 import BurgerIngredients from './components/BurgerIngredients/BurgerIngredients';
+import BurgerIngredient from './components/BurgerIngredient/BurgerIngredient';
 import AppHeader from './components/AppHeader/AppHeader.js';
+import AppStyle from './App.module.css';
 import { getResponseData } from './utils/api';
-import './App.css';
 
 function App() {
-  const [data, setData] = useState(null);
+  const [dataArray, setDataArray] = useState(null);
   useEffect(() => {
-    setData(getResponseData());
+    getResponseData(setDataArray);
   }, []);
 
-  return (
-    <div className="App">
-      <AppHeader />
-      <div>test</div>
-      <BurgerIngredients />
-      <BurgerConstructor />
-    </div>
-  );
+  if (dataArray) {
+    return (
+      <div className={AppStyle.App}>
+        <AppHeader />
+        <main className={AppStyle.main}>
+          <BurgerIngredients>
+            <h2 className={AppStyle.typeIngredient + ' pt-10 pb-6'} id="bun">
+              Булки
+            </h2>
+            {returnCardIngredient(dataArray.data, 'bun')}
+            <h2 className={AppStyle.typeIngredient + ' pt-10 pb-6'} id="sauce">
+              Соусы
+            </h2>
+            {returnCardIngredient(dataArray.data, 'sauce')}
+            <h2 className={AppStyle.typeIngredient + ' pt-10 pb-6'} id="main">
+              Начинки
+            </h2>
+            {returnCardIngredient(dataArray.data, 'main')}
+          </BurgerIngredients>
+
+          <BurgerConstructor />
+        </main>
+      </div>
+    );
+  } else {
+    return <h2>Loading</h2>;
+  }
+}
+
+function returnCardIngredient(arr, typeIngredient) {
+  return arr
+    .filter(data => data.type === typeIngredient)
+    .map(data => <BurgerIngredient key={data._id} {...data} />);
 }
 
 export default App;
