f4a5f64000081f6f6db1e943e49369bfb93107f1
Natalya Shymkova <Natalia.Matiishena@yandex.ru>
2023-01-26 22:02:59 +0300

    Ingredient constructor designed

--- src/components/BurgerIngredient/BurgerIngredient.js
+++ src/components/BurgerIngredient/BurgerIngredient.js
@@ -4,14 +4,18 @@ import BurgerIngredientStyle from './BurgerIngredient.module.css';
 function BurgerIngredient(props) {
   const { name, price, image, type } = props;
   return (
-    <article type={type} className={BurgerIngredientStyle.card + ' pl-4'}>
+    <article type={type} className={BurgerIngredientStyle.card + ' pl-4 mt-6'}>
       <img src={image} alt={name} className={BurgerIngredientStyle.img + ' pl-4 pr-4'} />
-      <span className={BurgerIngredientStyle.calculator + ' text text_type_digits-default'}>1</span>
+      <span
+        className={BurgerIngredientStyle.calculator + ' text text_type_digits-default mt-0 mb-0'}
+      >
+        1
+      </span>
       <div className={BurgerIngredientStyle.info + ' pt-1 pb-1'}>
         <span className="text text_type_digits-default">{price}</span>
         <CurrencyIcon type="primary" />
       </div>
-      <span className="text text_type_main-default">{name}</span>
+      <span className={BurgerIngredientStyle.info + ' text text_type_main-default'}>{name}</span>
     </article>
   );
 }
--- src/components/BurgerIngredients/BurgerIngredients.js
+++ src/components/BurgerIngredients/BurgerIngredients.js
@@ -3,27 +3,30 @@ import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import BurgerIngredientStyle from './BurgerIngredients.module.css';
 
 function BurgerIngredients(props) {
-  console.log(props);
   const [current, setCurrent] = useState('bun');
+  const handleTabClick = current => {
+    setCurrent(current);
+    document.getElementById(current).scrollIntoView({ block: 'start' });
+  };
+
   return (
     <section className={BurgerIngredientStyle.box}>
-      <h1 className="text text_type_main-large pb-5 pt-5">Соберите бургер</h1>
-      <div className={BurgerIngredientStyle} style={{ display: 'flex' }}>
-        <Tab value="bun" active={current === 'bun'} onClick={setCurrent} href="#bun">
-          <a className={BurgerIngredientStyle.link}>Булки</a>
+      <h1 className="text text_type_main-large pb-5 pt-10">Соберите бургер</h1>
+      <div style={{ display: 'flex' }}>
+        <Tab value="bun" active={current === 'bun'} onClick={handleTabClick}>
+          Булки
         </Tab>
-        <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
-          <a href="#sauce" className={BurgerIngredientStyle.link}>
-            Соусы
-          </a>
+        <Tab value="sauce" active={current === 'sauce'} onClick={handleTabClick}>
+          Соусы
         </Tab>
-        <Tab value="main" active={current === 'main'} onClick={setCurrent}>
-          <a href="#main" className={BurgerIngredientStyle.link}>
-            Начинки
-          </a>
+        <Tab value="main" active={current === 'main'} onClick={handleTabClick}>
+          Начинки
         </Tab>
       </div>
-      <section className={BurgerIngredientStyle.ingredients + ' custom-scroll'}>
+      <section
+        id="sectionIngredients"
+        className={BurgerIngredientStyle.ingredients + ' custom-scroll pr-1'}
+      >
         {props.children}
       </section>
     </section>
