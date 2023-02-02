54c664492918c2537c4668fbdb53f45042724f42
Natalya Shymkova <Natalia.Matiishena@yandex.ru>
2023-02-03 01:35:14 +0300

    rename style app header

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -4,25 +4,25 @@ import {
   ListIcon,
   ProfileIcon
 } from '@ya.praktikum/react-developer-burger-ui-components';
-import appHeaderStyle from './AppHeader.module.css';
+import AppHeaderStyle from './AppHeader.module.css';
 
 function AppHeader() {
   return (
-    <header className={appHeaderStyle.header}>
-      <section className={appHeaderStyle.container}>
+    <header className={AppHeaderStyle.header}>
+      <section className={AppHeaderStyle.container}>
         <nav className="pb-4 pt-4">
-          <ul className={appHeaderStyle.navigation}>
-            <li className={appHeaderStyle.navigation__item + ' pb-4 pt-4 pr-5 pl-5'}>
+          <ul className={AppHeaderStyle.navigation}>
+            <li className={AppHeaderStyle.navigation__item + ' pb-4 pt-4 pr-5 pl-5'}>
               <BurgerIcon type="primary" />
               <span className="text text_type_main-default pl-2">Конструктор</span>
             </li>
-            <li className={appHeaderStyle.navigation__item + ' pb-4 pt-4 pr-5 pl-5'}>
+            <li className={AppHeaderStyle.navigation__item + ' pb-4 pt-4 pr-5 pl-5'}>
               <ListIcon type="primary" />
               <span className="text text_type_main-default pl-2">Лента заказов</span>
             </li>
           </ul>
         </nav>
-        <div className={appHeaderStyle.logo}>
+        <div className={AppHeaderStyle.logo}>
           <Logo />
         </div>
 
