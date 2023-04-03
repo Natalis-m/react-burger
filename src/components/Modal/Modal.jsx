324a6d86083c6872da48a6c671685ed3f84b0026
Natalya <natalia.matiishena@yandex.ru>
2023-04-03 20:54:43 +0300

    fix modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,47 +4,6 @@ import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import modalStyle from './Modal.module.css';
 
 const modalElement = document.querySelector('#modal');
-
-// const Modal = ({ onClose, children }) => {
-// const Modal = () => {
-//   const element = document.createElement('div');
-
-//   useEffect(modalElement.appendChild(element), [element]);
-
-//   // useEffect(() => {
-//   //   function closeByEscape(evt) {
-//   //     if (evt.key === 'Escape') {
-//   //       onClose(false);
-//   //     }
-//   //   }
-//   //   document.addEventListener('keydown', closeByEscape);
-//   //   return () => {
-//   //     document.removeEventListener('keydown', closeByEscape);
-//   //   };
-//   // }, []);
-
-//   return createPortal(
-//     <section
-//       className={modalStyle.modal}
-//       // onClick={() => onClose(false)}
-//     >
-//       <div
-//         className={modalStyle.content + ' pt-10 pr-10 pb-10 pl-10'}
-//         onClick={e => e.stopPropagation()}
-//       >
-//         <div
-//           className={modalStyle.closed}
-//           // onClick={() => onClose(false)}
-//         >
-//           <CloseIcon type="primary" />
-//         </div>
-//         {/* {children} */}
-//       </div>
-//     </section>,
-//     element
-//   );
-// };
-
 const Modal = ({ onClose, children }) => {
   useEffect(() => {
     function closeByEscape(evt) {
