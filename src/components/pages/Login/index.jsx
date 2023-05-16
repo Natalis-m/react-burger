c921f7b3e16ae849555951274d610b3d2977b6bf
Natalya <natalia.matiishena@yandex.ru>
2023-05-16 22:07:53 +0300

    styling adjustments

--- src/components/pages/Login/index.jsx
+++ src/components/pages/Login/index.jsx
@@ -1,27 +1,50 @@
 // страница авторизации, открывается из App
-import {
-  Button,
-  EmailInput,
-  PasswordInput
-} from '@ya.praktikum/react-developer-burger-ui-components';
+import { Button, Input } from '@ya.praktikum/react-developer-burger-ui-components';
 import { Link } from 'react-router-dom';
 import Style from './LoginStyle.module.css';
+import { useState, useRef } from 'react';
 
 function Login() {
+  const [user, setUser] = useState({ email: '', password: '' });
+  const inputRef = useRef(null);
+  const onIconClick = () => {
+    setTimeout(() => inputRef.current.focus(), 0);
+    alert('Icon Click Callback');
+  };
+
+  const update = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    });
+    console.log({ user });
+  };
+
   return (
     <section className={Style.content}>
       <div className={Style.form}>
         <h2 className="text text_type_main-medium">Вход</h2>
-        <EmailInput
-          onChange=""
-          value=""
+        <Input
+          type={'email'}
+          onChange={update}
+          value={user.email}
           name={'email'}
           placeholder="E-mail"
-          isIcon={false}
+          icon={false}
           extraClass="mb-2"
         />
-        <PasswordInput onChange="" value="" name={'password'} extraClass="mb-2" />
-        <Button htmlType="button" type="primary" size="medium" extraClass="ml-2">
+        <Input
+          type={'password'}
+          onChange={update}
+          value={user.password}
+          name={'password'}
+          placeholder="Пароль"
+          icon={'HideIcon'}
+          extraClass="mb-2"
+          ref={inputRef}
+          onIconClick={onIconClick}
+        />
+        <Button htmlType="button" type="primary" size="medium" onChange="" extraClass="ml-2">
           Войти
         </Button>
       </div>
