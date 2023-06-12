c48d6d233d0a93c98f852c56359decd62a0442bc
Natalya <natalia.matiishena@yandex.ru>
2023-06-12 03:02:49 +0300

    authorization check

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,8 +1,10 @@
+import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
 function ProtectedRoute({ element }) {
-  const isUserLoaded = localStorage.getItem('user');
+  const isUserLoaded = useSelector(state => state.userReducer.user.name);
 
+  console.log('isUserLoaded', isUserLoaded);
   return isUserLoaded ? element : <Navigate to="/login" replace />;
 }
 
