2133aa3df2dbd3ba6815d2b9b7b4a212b7db01d1
Natalya <natalia.matiishena@yandex.ru>
2023-06-21 14:22:22 +0300

    declare module

--- src/style.css.d.ts
+++ src/style.css.d.ts
@@ -2,3 +2,7 @@ declare module '*.css' {
   const exports: { [exportName: string]: string };
   export = exports;
 }
+declare module '*.png' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
