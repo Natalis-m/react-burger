b20a10f8244334536c1043a03c9e6ef43e21dc26
Natalya <natalia.matiishena@yandex.ru>
2023-06-21 14:20:21 +0300

    hook typing

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+export function useForm<T>(inputValues: T) {
+  const [values, setValues] = useState<T>(inputValues);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  return { values, handleChange, setValues };
+}
